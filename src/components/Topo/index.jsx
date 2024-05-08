c4399076a207790f70c3728093bd6c087dc2d87a
unknown <fabioflauzino@hotmail.com>
2024-05-08 02:50:57 -0300

    criando header e capa

--- src/components/Topo/index.jsx
+++ src/components/Topo/index.jsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import styles from './Topo.module.css'
 import Link from 'next/link'
+import Logo from '../../../public/logo.png'
 
 export default function Topo () {
   return (
     <header className={styles.container_topo}>
       <div className={`container ${styles.topo_content}`}>
-        <Image src={''} alt='Logo' title='Logo'/>
+        <Image src={Logo} alt='Logo' title='Logo' className={styles.img_logo}/>
         <nav className={styles.links}>
           <Link href='#'>Produtos</Link>
           <Link href='#'>Sobre</Link>
